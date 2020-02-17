5b919766c49caccf446f14101cacb7064700a2c9
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2020-02-17 04:21:30 +0200

    First Commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+	@IBOutlet weak var ball: UIImageView!
+	
+	@IBAction func buttonPressed(_ sender: Any) {
+		ball.image = ballArray.randomElement()
+	}
 }
 
